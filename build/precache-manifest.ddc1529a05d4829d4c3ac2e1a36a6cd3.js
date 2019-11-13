f7067b0148f71c401e6512ea63d101dfdd0234d9
Sandeep Kumar <sandeep.1310128@kiet.edu>
2019-11-13 20:10:05 +0530

    fix

---
+++ build/precache-manifest.ddc1529a05d4829d4c3ac2e1a36a6cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1371a0f62a31973b65d5fa50dc2bb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7491cc02e231cbcd917b",
+    "url": "/static/css/main.1047da8d.chunk.css"
+  },
+  {
+    "revision": "7741c429421e88cae0e4",
+    "url": "/static/js/2.be2973dd.chunk.js"
+  },
+  {
+    "revision": "7491cc02e231cbcd917b",
+    "url": "/static/js/main.54cafa3c.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
